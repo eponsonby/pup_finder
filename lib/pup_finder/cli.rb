4e027d00f8c5afb9c8b780e5cc9d66ed198a7c4f
github username <github email address>
2019-06-07 09:56:10 -0400

    Add loop

--- lib/pup_finder/cli.rb
+++ lib/pup_finder/cli.rb
@@ -8,8 +8,35 @@ class CLI
         welcome
         user_input = get_input
         selected_size = generate_url(user_input).join
-        AKCScraper.get_breeds(selected_size)
-        # list_small_breeds
+        breed_scraper = AKCScraper.new
+        breed_scraper.get_breeds(selected_size)
+        breed_scraper.make_breeds
+        loop do 
+            intermediate_message
+            list_breeds
+            enter_a_number
+            number_entered = get_number
+            more_info(number_entered)
+            continue_message
+            yes_or_no = continue_gets
+                if yes_or_no == "yes"
+                    
+                elsif yes_or_no != "yes" && yes_or_no != "no"
+                    loop do
+                        if yes_or_no != "yes" && yes_or_no != "no"
+                            puts "Please try again"
+                            yes_or_no = continue_gets
+                        else 
+                            break
+                        end
+                    end
+                elsif yes_or_no == "yes"
+
+                elsif yes_or_no == "no"
+                    puts "\n\nGoodbye!"
+                break
+                end
+        end
     end
 
     def welcome
@@ -26,40 +53,89 @@ class CLI
         url_to_scrape = URLGenerator.new_url(input)  
     end
 
+    def intermediate_message
+        puts "There are #{Breed.all.length} breeds in this list"
+    end
 
-    # def menu
-    #     input = gets.chomp.downcase
-    #         case input
-    #         when "small"
-    #             #access the length of the list of dogs that are small
-    #             puts "There are length dogs in this list"
-    #             puts "Enter a letter from a-z to see all breeds by that letter"
-    #             letter = gets.chomp.downcase
-    #             # user will enter in a letter
-    #             # check to see if that letter is included in an array of all letters from a to z
-    #             # need to return a numbered list of all dogs that start with letter
-    #             # access all dog instances, access their breed names, access only those that start with letter 
-    #                 puts "Enter a number to see more info about that dog"
-    #                 puts "list of numbered a dogs"
-    #         when "medium"
-    #             puts "There are number dogs in this list"
-    #             puts "Enter a letter from a-z to see all breeds by that letter"
-    #             letter = gets.chomp.downcase
-    #         when "large"
-    #             puts "There are number dogs in this list"
-    #             puts "Enter a letter from a-z to see all breeds by that letter"
-    #             letter = gets.chomp.downcase
-    #         when "huge"
-    #             puts "There are number dogs in this list"
-    #             puts "Enter a letter from a-z to see all breeds by that letter"
-    #             letter = gets.chomp.downcase
-    #         end
-    # end
+    def breed_hash
+        breed_hash = {}
+        Breed.all.each_with_index do |breed, index|
+            breed_hash[index + 1] = breed.breed_name
+        end
+        breed_hash
+    end
+    
+    def list_breeds
+        puts "Here are the first 10 breeds. Enter next to see more"
+        breed_hash.each do |key, value|
+            puts "#{key}. #{value}"
+        end
+    end
 
-    # def list_small_breeds
-    #     @small_breeds = PupFinder::Breed.small
-    #     puts @small_breeds
-    # end
+    def enter_a_number
+        puts "\n\nEnter a number to see more information about that pup \n\n"
+    end
+
+    def get_number
+        number = gets.to_i
+            if number == 0
+                puts "Please try again"
+            elsif number 
+                # elsif yes_or_no != "yes" && yes_or_no != "no"
+                #     loop do
+                #         if yes_or_no != "yes" && yes_or_no != "no"
+                #             puts "Please try again"
+                #             yes_or_no = continue_gets
+                #         else 
+                #             break
+                #         end
+                #     end
+                # elsif yes_or_no == "yes"
+
+                # elsif yes_or_no == "no"
+                #     puts "\n\nGoodbye!"
+                # break
+                # end
+    end
+
+    def more_info(number_entered)
+        #if the user enters 4
+        # list more info about dog 4 name, weight, life ex, description
+        breed_to_see = breed_hash[number_entered]
+        Breed.all.each do |breed|
+            if breed.breed_name == breed_to_see
+                puts "\nThe #{breed.breed_name}\n\n"
+                puts "      __\n(___()'`;\n/,    /`\n\\\\'--\\\\"
+                puts "\nTemperament: #{breed.temperament}\n\n"
+                puts "Weight: #{breed.weight}\n\n"
+                puts "Life Expectancy: #{breed.life_expectancy}\n\n"
+                puts "Description: #{breed.description}"
+            end
+        end
+    end
+
+    def continue_message
+        puts "\nWould you like to see another pup?"
+        puts "You can enter yes or no"
+        puts "Or, you can enter menu to go back to the main menu\n\n"
+    end
+
+    def continue_gets
+        yes_or_no = gets.strip.downcase
+    end
+
+    def run_again(yes_or_no)
+        if yes_or_no == "yes"
+            list_breeds
+            enter_a_number
+            entered = get_number
+            more_info(entered)
+            continue_message
+        else
+            puts "Hope you found a pup!"
+        end
+    end
+
 
 end
 
--- lib/pup_finder/scraper.rb
+++ lib/pup_finder/scraper.rb
@@ -1,44 +1,65 @@
 class AKCScraper
     @@all_breeds = []
 
-    def self.get_breeds(url)
-        
+    def get_breeds(url)
          # open first url based on user input
         doc = Nokogiri::HTML(open(url))
 
         # get all breeds on page 1
-        @@all_breeds << first_page = doc.css('.breed-card-type-grid .grid-col')
-
+        first_page_breeds = doc.css('.breed-card-type-grid .grid-col')
+        first_page_breeds.each do |breed|
+            breed_name = breed.css(".breed-type-card a h3").text
+            breed_link = breed.css(".breed-type-card a").attribute('href').value
+            Breed.new(breed_name, breed_link)
+        end
         # get load more link on page 1
         load_more_link = doc.css(".mt2 a").attribute("href")
         link_text = load_more_link.value
         
-        while load_more_link != []
-            link_text
+        while load_more_link != [] do
         # get all breeds on page 2
         next_page_html = Nokogiri::HTML(open(link_text))
-        @@all_breeds << next_page_breeds = next_page_html.css('.breed-card-type-grid .grid-col')
+        next_page_breeds = next_page_html.css('.breed-card-type-grid .grid-col')
+        
+        next_page_breeds.each do |breed|
+            breed_name = breed.css(".breed-type-card a h3").text
+            breed_link = breed.css(".breed-type-card a").attribute('href').value
+            Breed.new(breed_name, breed_link)
+        end
+
         # get load more link on page 2
         load_more_link = next_page_html.css(".mt2 a").attribute("href")
             if load_more_link
-                link_text = load_more_link .value
+                link_text = load_more_link.value
             else
                 load_more_link = []
             end
         end
-    binding.pry
+        
     end
     
-    # def make_breeds
-    #     self.get_breeds.each do |block|
-    #         breed = Breed.new
-    #         breed.name = 
-    #         breed.temperament =
-    #         breed.size =
-    #         breed.weight =
-    #         breed.life_expectancy =
-    #         breed.description =
-    #         breed.url =
-    # end
+
+    def make_breeds
+       Breed.all.each do |breed|
+        doc = Nokogiri::HTML(open(breed.breed_link))
+        # get each li and set each attribute
+        all_lis = doc.css(".panel-flex__aside ul li.attribute-list__row")
+            all_lis.each do |li|
+                if li.css(".attribute-list__term").text == "Temperament:" 
+                    breed.temperament = li.css(".attribute-list__description").text
+                elsif li.css(".attribute-list__term").text == "Weight:" 
+                    breed.weight = li.css(".attribute-list__description").text
+                elsif li.css(".attribute-list__term").text == "Life Expectancy:"
+                    breed.life_expectancy = li.css(".attribute-list__description").text.strip
+                end
+            end
+        #breed description
+        if doc.css(".breed-hero__footer")
+            breed.description = doc.css(".breed-hero__footer").text.strip
+        else
+            breed.description = "none available"
+        end
+       end
+    end
 end
 
