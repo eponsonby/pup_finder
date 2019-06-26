26a93b6963aa515173a6e726a5ce51955f9822ef
Erin Ponsonby <erinponsonby@gmail.com>
2019-06-26 12:33:21 -0400

    remove commented out lines

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,4 @@
-# require 'bundler/setup'
-# Bundler.require(:default)
 require 'open-uri'
-# require_all 'lib'
 require_relative '../lib/pup_finder/breed.rb'
 require_relative '../lib/pup_finder/cli.rb'
 require_relative '../lib/pup_finder/scraper.rb'
